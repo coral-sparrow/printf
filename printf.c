4fbc4ee8fb05b46cc589f45663b0a2572078d8d4
Mahmoud Gomaa <141342087+coral-sparrow@users.noreply.github.com>
2023-10-17 13:13:59 +0300

    online

--- printf.c
+++ printf.c
@@ -174,11 +174,11 @@ int _printf(const char *format, ...)
 		{
 			tmp_s = va_arg(ap, char *);
 			concat_buff(&b, tmp_s, 0);
-		} else if (tmp == 'd') || (tmp == 'i'))
+		} /* else if (tmp == 'd') || (tmp == 'i'))
 		{
 			tmp_d = (tmp == 'd') ? (va_arg(ap, int)) : (va_arg(ap, unsigned int))
 			concat_buff(&b, &tmp_d, 1);
-		}
+		} */
 		if ((i + 1) != n)
 			(tmp != '%') ? concat_slice(&b, format, idx[i],  idx[i + 1]) :
 				(concat_slice(&b, format, idx[i],  idx[i + 2]), i++);
